39c484ebec6ee83e3dcee4c09e425a7fcfd699d3
yyosifov <cypressx@gmail.com>
2014-10-14 22:40:40 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var TemperatureMeasurer = require('./TemperatureMeasurer.js');
 
+// Start measuring the Temperature, using the values provided from PIN A0 and measure every 10 seconds (10000 milliseconds)
 var temperatureMeasurer = new TemperatureMeasurer('A0', 10000);
 
 temperatureMeasurer.on('temperature.measured', function(value) {
